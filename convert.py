ae272c35b78fe400f014e68a345658939912e73a
Christoph Powazny <dnw_mailorder@gmx.de>
2019-12-15 19:36:18 +0100

    add convert functions for converting class to description and the other way around

---
+++ convert.py
@@ -0,0 +1,26 @@
+convert_label_to_description = {
+    "c0": "safe driving",
+    "c1": "texting - right",
+    "c2": "talking on the phone - right",
+    "c3": "texting - left",
+    "c4": "talking on the phone - left",
+    "c5": "operating the radio",
+    "c6": "drinking",
+    "c7": "reaching behind",
+    "c8": "hair and makeup",
+    "c9": "talking to passenger"
+}
+
+
+convert_description_to_label = {
+    "safe driving": "c0",
+    "texting - right": "c1",
+    "talking on the phone - right": "c2",
+    "texting - left": "c3",
+    "talking on the phone - left": "c4",
+    "operating the radio": "c5",
+    "drinking": "c6",
+    "reaching behind": "c7",
+    "hair and makeup": "c8",
+    "talking to passenger": "c9"
+}
